a203332656c4c1084a16ed8ab72b2c8250cb50d8
Yuriy Yarvinen <yuriy.yarvi@gmail.com>
2026-06-11 13:01:39 +0300

    return func as value

--- functions/main.go
+++ functions/main.go
@@ -3,6 +3,7 @@ package main
 func main() {
 
 	numbers := []int{1, 2, 3, 4, 5}
+	moreNumbers := []int{6, 7, 8, 9, 10}
 
 	println("Original numbers:")
 	for _, number := range numbers {
@@ -23,6 +24,22 @@ func main() {
 		println(number)
 	}
 
+	funcForNumbers := getTransformersFunction(&numbers)
+	transformedNumbers = transformNumbers(&numbers, funcForNumbers)
+
+	println("Transformed numbers using function from getTransformersFunction:", funcForNumbers)
+	for _, number := range transformedNumbers {
+		println(number)
+	}
+
+	funcForMoreNumbers := getTransformersFunction(&moreNumbers)
+	transformedMoreNumbers := transformNumbers(&moreNumbers, funcForMoreNumbers)
+
+	println("Transformed more numbers using function from getTransformersFunction:", funcForMoreNumbers)
+	for _, number := range transformedMoreNumbers {
+		println(number)
+	}
+
 }
 
 func transformNumbers(numbers *[]int, multiplierFunc func(int) int) []int {
@@ -33,6 +50,13 @@ func transformNumbers(numbers *[]int, multiplierFunc func(int) int) []int {
 	}
 
 	return transformed
+}
+
+func getTransformersFunction(numbers *[]int) func(n int) int {
+	if (*numbers)[0] == 1 {
+		return double
+	}
+	return triple
 }
 
 func double(n int) int {
