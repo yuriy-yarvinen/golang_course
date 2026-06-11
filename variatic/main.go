bbb7dec2e95ce9ce3be1bc456421902b570b09fb
Yuriy Yarvinen <yuriy.yarvi@gmail.com>
2026-06-11 22:45:15 +0300

    update

--- variatic/main.go
+++ variatic/main.go
@@ -2,9 +2,12 @@ package main
 
 func main() {
 
-	sum := sum(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20)
-	println("Sum:", sum)
+	sumOfNumbers := sum(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20)
+	println("Sum:", sumOfNumbers)
 
+	numbers := []int{1, 2, 3, 4, 5}
+	sumOfNumbersInSlice := sum(numbers...)
+	println("Sum of numbers:", sumOfNumbersInSlice)
 }
 
 func sum(numbers ...int) int {
