c9c72211e094da128831a917e2edb70a0484d0bf
Yuriy Yarvinen <yuriy.yarvi@gmail.com>
2026-06-30 00:40:57 +0300

    add write to file append

--- concurrency/main.go
+++ concurrency/main.go
@@ -8,17 +8,31 @@ import (
 
 func greet(phrase string, channel chan bool) {
 	fmt.Println("hello", phrase)
+	WriteToFile(phrase)
 
-	err := os.WriteFile("test.txt", []byte(phrase), 664)
+	channel <- true
+}
+
+func WriteToFile(phrase string) {
+	file, err := os.OpenFile("data.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
 		fmt.Println(err)
 	}
-	channel <- true
+	defer file.Close()
+
+	_, err = file.WriteString(phrase + "\n")
+	if err != nil {
+		fmt.Println(err)
+	}
+	if err != nil {
+		fmt.Println(err)
+	}
 }
 
 func slowGreent(phrase string, channel chan bool) {
 	time.Sleep(3 * time.Second)
 	fmt.Println("hello", phrase)
+	WriteToFile(phrase)
 	channel <- true
 	close(channel)
 
