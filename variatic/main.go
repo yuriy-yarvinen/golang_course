6b95c16d06e843d9bd913a4d7fab3e9ab24a9481
Yuriy Yarvinen <yuriy.yarvi@gmail.com>
2026-07-16 01:17:42 +0300

    iota and bit shift

--- variatic/main.go
+++ variatic/main.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 func main() {
 
 	sumOfNumbers := sum(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20)
@@ -8,6 +10,23 @@ func main() {
 	numbers := []int{1, 2, 3, 4, 5}
 	sumOfNumbersInSlice := sum(numbers...)
 	println("Sum of numbers:", sumOfNumbersInSlice)
+
+	const (
+		_ = iota
+		a
+		b
+		c
+		d
+	)
+
+	fmt.Printf("%d \t %b \n ", 1, 1)
+	fmt.Printf("%d \t %b \n ", 1<<a, 1<<a)
+	fmt.Printf("%d \t %b \n ", 1<<b, 1<<b)
+	fmt.Printf("%d \t %b \n ", 1<<c, 1<<c)
+	fmt.Printf("%d \t %b \n ", 1<<d, 1<<d)
+	fmt.Printf("%d \t %b \n ", 3, 3)
+	fmt.Printf("%d \t %b \n ", 1<<50, 1<<50)
+	fmt.Printf("%d \t %b \n ", 1125899906842625, 1125899906842625)
 }
 
 func sum(numbers ...int) int {
